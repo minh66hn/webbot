bd08f82b9ec8b4862e51c3de875ce759adcb6c37
ngo van minh <62501050+minh66hn@users.noreply.github.com>
2021-05-25 15:12:43 +0700

    Add files via upload

---
+++ src/containers/Docs/index.tsx
@@ -0,0 +1,38 @@
+import DataTable from "react-data-table-component";
+import { DocsWrapper } from "./styled";
+
+import { commands } from "./data";
+
+const Docs = () => {
+  const columns = [
+    {
+      name: "ID",
+      selector: "id",
+    },
+    {
+      name: "Name",
+      selector: "name",
+    },
+    {
+      name: "Description",
+      selector: "description",
+    },
+    {
+      name: "Usage",
+      selector: "usage",
+    },
+  ];
+
+  return (
+    <DocsWrapper>
+      <DataTable
+        title="VanMingBot's commands"
+        columns={columns}
+        data={commands}
+        // customStyles={customStyles}
+      />
+    </DocsWrapper>
+  );
+};
+
+export default Docs;
